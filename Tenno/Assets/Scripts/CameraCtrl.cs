a68049646be820eda34e469ca8c6796b7ca19452
GuangLingZhiXi <90486715@qq.com>
2020-11-22 16:34:23 +0900

    第一回

---
+++ Tenno/Assets/Scripts/CameraCtrl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraCtrl : MonoBehaviour
+{
+
+    private Transform Cameratransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cameratransform = GameObject.Find("Camera").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Rotate(-Input.GetAxis("Mouse Y"), 0 ,0 );
+    }
+}
---
+++ Tenno/Assets/Scripts/PlayerCtrl.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCtrl : MonoBehaviour
+{
+
+    private Transform Playertransform;
+    private Rigidbody PlayrRigidbody;
+    private float h=0;
+    private float v = 0;
+    public float speed = 0;
+    [SerializeField]  private bool IsinAir;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Playertransform = GameObject.Find("unitychan").GetComponent<Transform>();
+        PlayrRigidbody = GameObject.Find("unitychan").GetComponent<Rigidbody>();
+        speed = 0.1f;
+        IsinAir = false;
+    
+}
+
+// Update is called once per frame
+void Update()
+    {
+        h = Input.GetAxis("Horizontal") ;
+        v = Input.GetAxis("Vertical") ;
+        Vector3 direction = new Vector3(h, 0, v);
+        
+        this.transform.Translate(direction * speed);
+        this.transform.Rotate(0,Input.GetAxis("Mouse X"),0);
+        Jump();
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == ("Plane"))
+        {
+            Debug.Log("aaaaaaaaaaaaa");
+            IsinAir = false;
+        }
+    }
+    void Jump()
+    {
+        Vector3 JumpDirection = new Vector3(0, 10.0f, 0);
+        if (IsinAir == false&&Input.GetKey(KeyCode.Space))
+        {
+
+            IsinAir = true;
+            PlayrRigidbody.AddForce(JumpDirection);
+
+        }
+    }
+}
