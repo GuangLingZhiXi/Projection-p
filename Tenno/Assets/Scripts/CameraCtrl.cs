06a8d47acdaf167af99e7a2c612d2d815a156f0a
GuangLingZhiXi <904867015@qq.com>
2020-12-19 17:50:22 +0900

    第五回　宿題

--- Tenno/Assets/Scripts/CameraCtrl.cs
+++ Tenno/Assets/Scripts/CameraCtrl.cs
@@ -12,6 +12,7 @@ public class CameraCtrl : MonoBehaviour
     public Vector3 CameraDir=new Vector3(0,0,0);
 
 
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,12 +23,12 @@ public class CameraCtrl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        
         //if (>)
         //{
-   
 
-      
+
+
     }
 
 
--- Tenno/Assets/Scripts/PlayerCtrl.cs
+++ Tenno/Assets/Scripts/PlayerCtrl.cs
@@ -5,14 +5,32 @@ using UnityEngine;
 
 public class PlayerCtrl : MonoBehaviour
 {
+    public enum States
+    {
+        Grounded,
+        AtWall,
+        AirDownWall,
+        Air,
+        Hang
+    }
     public Animator PlayerAnimator;
     private Transform Playertransform;
     private Rigidbody PlayerRigidbody;
     private float h = 0;
     private float v = 0;
     public float speed = 0;
-    [SerializeField] private bool IsGround;
-    private bool stop = false;
+    public States PlayerStates;
+    public Camera PlayerCamera;
+
+    private Ray HitRayA;
+    private Ray HitRayB;
+    public Vector3 PlusPositionA = new Vector3(0, 1.0f, 0);
+    public Vector3 PlusPositionB = new Vector3(0, 1.5f, 0);
+    private RaycastHit HitDetectA;
+    private RaycastHit HitDetectB;
+
+    //[SerializeField] private bool IsGround;
+    //private bool stop = false;
 
     // Start is called before the first frame update
     void Start()
@@ -22,53 +40,64 @@ public class PlayerCtrl : MonoBehaviour
         Playertransform = GameObject.Find("unitychan").GetComponent<Transform>();
         PlayerRigidbody = GameObject.Find("unitychan").GetComponent<Rigidbody>();
         //speed = 0.1f;
-        IsGround = true;
-
+        //IsGround = true;
+        PlayerStates = States.Grounded;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+       // Vector3 Position = ;
+         HitRayA = new Ray(transform.position + PlusPositionA, transform.forward);
+
+         HitRayB = new Ray(transform.position + PlusPositionB, transform.forward);
+        if(Physics.Raycast(HitRayA, out HitDetectA, 0.3f) && !Physics.Raycast(HitRayB, out HitDetectB, 0.3f))
+        {
+            PlayerStates = States.Hang;
+            PlayerRigidbody.useGravity = false;
+            Debug.Log("Hang");
+        }
+        
+       Debug.DrawRay(HitRayA.origin, HitRayA.direction, Color.blue);
+        Debug.DrawRay(HitRayB.origin, HitRayB.direction, Color.yellow);
         CharacterInput();
-        if (stop == true)
-        {
-            if (v < 0)//後退
-            {
-                stop = false;
-            }
-            if (v >= 0)
-            {
-                PlayerAnimator.SetFloat("Speed", 3f);//SPEEDが3になればW押しても動けなくなる
-                Rotate();
-            }
-            if (h != 0)//水平移動
-            {
-                PlayerAnimator.SetFloat("Speed", 1f);
-                PlayerAnimator.SetFloat("Direction", h);
+        //if (stop == true)
+        //{
+        //    if (v < 0)//後退
+        //    {
+        //        stop = false;
+        //    }
+        //    if (v >= 0)
+        //    {
+        //        PlayerAnimator.SetFloat("Speed", 3f);//SPEEDが3になればW押しても動けなくなる
+        //        Rotate();
+        //    }
+        //    if (h != 0)//水平移動
+        //    {
+        //        PlayerAnimator.SetFloat("Speed", 1f);
+        //        PlayerAnimator.SetFloat("Direction", h);
 
-                //  commit
-                //
-                Walk();
-                WalkAnim();
+        //        //  commit
+        //        //
+        //        Walk();
+        //        WalkAnim();
                 
-            }
+        //    }
 
 
 
-        }
-        else
-        {
-            Walk();
-            WalkAnim();
-            Rotate();
-        }
+        //}
 
+        Move();
+ 
+        PlayAnim();
 
         Squad();
 
         Rotate();
 
-        Jump();
+        JumpOrClimb();
 
     }
 
@@ -84,7 +113,7 @@ public class PlayerCtrl : MonoBehaviour
         //Debug.Log(h);
     }
 
-    void Walk()
+    void Move()
     {
 
 
@@ -107,14 +136,44 @@ public class PlayerCtrl : MonoBehaviour
         }
     }
 
-    void WalkAnim()
+    void JumpOrClimb()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                if (PlayerStates == States.AtWall)
+                {
+                    Debug.Log("Climb");
+                    Vector3 ClimbDirection = new Vector3(0, 15000f, 0);
+                    PlayerRigidbody.AddForce(ClimbDirection);
+                    PlayerStates = States.AirDownWall;
+                }
+
+            }
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if (PlayerStates == States.Grounded || PlayerStates == States.AtWall)
+            {
+                Debug.Log("Jump");
+                Vector3 JumpDirection = new Vector3(0, 10000f, 0);
+                PlayerRigidbody.AddForce(JumpDirection);
+
+                PlayerAnimator.SetBool("Jump", true);
+                PlayerStates = States.Air;
+            }
+
+        }
+
+    }
+    void PlayAnim()
     {
         if (PlayerAnimator)
         {
             if (v != 0)
             {
                 PlayerAnimator.SetFloat("Speed", v);
-              
             }
             if (h != 0)
             {
@@ -136,7 +195,15 @@ public class PlayerCtrl : MonoBehaviour
     {
         if (collision.collider.tag == "Plane")
         {
-            IsGround = true;
+            if(PlayerStates == States.AirDownWall)
+            {
+                PlayerStates = States.AtWall;
+            }else if(PlayerStates == States.Air)
+            {
+                PlayerStates = States.Grounded;
+            }
+            
+            //IsGround = true;
             PlayerAnimator.SetBool("Jump", false);
         }
 
@@ -149,8 +216,11 @@ public class PlayerCtrl : MonoBehaviour
         if (other.tag == "Wall")
         {
             Debug.Log("1");
-
-            stop = true;
+            if(PlayerStates == States.Grounded)
+            {
+                PlayerStates = States.AtWall;
+            }
+           // stop = true;
 
         }
     }
@@ -159,27 +229,13 @@ public class PlayerCtrl : MonoBehaviour
     {
         if (other.tag == "Wall")
         {
-            //Debug.Log("1");
-
-            stop = false;
-
-        }
-    }
-
-    void Jump()
-    {
-        if (Input.GetKey(KeyCode.Space) && IsGround == true)
-        {
-
-            Vector3 JumpDirection = new Vector3(0, 10.0f, 0);
-            PlayerRigidbody.AddForce(JumpDirection);
-            IsGround = false;
-            PlayerAnimator.SetBool("Jump", true);
-
-            //PlayrRigidbody.AddForce(JumpDirection);
-
-
+            if (PlayerStates == States.AtWall)
+            {
+                PlayerStates = States.Grounded;
+            }
 
         }
     }
+
+
 }
